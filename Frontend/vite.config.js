2c7085a3566970d7d4123fbb9bd5a8b06ab00540
Saloni Goyal <salonigoyal2709@gmail.com>
2025-07-31 17:13:39 +0530

    added vercel.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',          // relative paths for assets
   plugins: [react()],
+  build: {
+    outDir: 'dist'     // make sure this matches your vercel.json
+  }
 })
